71604f037e404e76bc957406acb0a591cba36cde
Александр Макаров <alexmk.work@gmail.com>
2022-10-21 13:15:08 +0500

    Страница логина

--- projects/mockup-to-html/pages/login/script.js
+++ projects/mockup-to-html/pages/login/script.js
@@ -1,6 +1,43 @@
 "use strict"
 
-// let a = document.querySelector('.sport-card');
-// let b = document.querySelector(':root');
+// setInterval(() => {
+//     console.log(document.documentElement.clientHeight,
+//         document.documentElement.clientWidth);
+// }, 500);
 
-// console.log(window.getComputedStyle(a).getPropertyValue('width'));
+
+// window.onkeydown = function(evt) {
+//     if (evt.key === 'k') {
+//         let clientWidth = html.getBoundingClientRect().width;
+//         console.log(clientWidth);
+//     }
+// }
+
+
+// let logo = document.querySelector('.header__inner');
+// let html = document.querySelector('html');
+
+
+
+
+// window.onresize = function(evt) {
+
+// }
+
+
+// setInterval(() => {
+//     let clientWidth = html.getBoundingClientRect().width;
+//     let width = logo.getBoundingClientRect().width;
+
+//     console.log(width)
+
+//     let ratio = width / clientWidth;
+
+//     let scaleValue = 0.35 / ratio;
+
+//     if (ratio > 0.35) {
+//         logo.style.transform = `scale(${scaleValue})`;
+//     } else {
+//         logo.style.transform = '';
+//     }
+// }, 100);
